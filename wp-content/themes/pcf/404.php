4c3d685b3cf20a71568c1e9c4e3d303e61a24677
Jonas Belcina <jonasbelcina10@gmail.com>
2016-03-07 15:17:17 +0400

    Fix 404

--- wp-content/themes/pcf/404.php
+++ wp-content/themes/pcf/404.php
@@ -12,23 +12,47 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
+			<section class="error-404 not-found about-us">
+				<div class="container">
+					<h2><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h2>
+				</div>
 			</section><!-- .error-404 -->
 
 		</main><!-- .site-main -->
 
-		<?php get_sidebar( 'content-bottom' ); ?>
-
 	</div><!-- .content-area -->
 
-<?php get_sidebar(); ?>
+	<section class="consultants about-us">
+		<div class="container">
+			<h2>Talk To Our Consultants</h2>
+			<p>Our experts have vast industry experience to understand and suggest the best solutions for you</p>
+			<a href="" data-toggle="modal" data-target="#contact_popup">Contact Us</a>
+		</div>
+	</section>
+
+	<section class="contact-icons about-us">
+		<div class="container">
+			<div class="col-sm-4 contact-icon-col">
+				<div class="contact-icon-wrap">
+					<h2>Office</h2>
+					<h3>Bldg 7, Street N606, JAFZA Dubai</h3>
+				</div>
+			</div>
+
+			<div class="col-sm-4 contact-icon-col">
+				<div class="contact-icon-wrap">
+					<h2>Phone</h2>
+					<h3><?php the_field('phone' , 'options'); ?></h3>
+				</div>
+			</div>
+
+			<div class="col-sm-4 contact-icon-col">
+				<div class="contact-icon-wrap">
+					<h2>E-mail</h2>
+					<h3><a href="mailto:<?php the_field('email' , 'options'); ?>"><?php the_field('email' , 'options'); ?></a></h3>
+				</div>
+			</div>
+		</div>
+	</section>
+
 <?php get_footer(); ?>
