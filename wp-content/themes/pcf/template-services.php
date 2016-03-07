d6d9d0299cd394d46f89db3ad0fc71e6d70651ca
Jonas Belcina <jonasbelcina10@gmail.com>
2016-03-07 11:30:29 +0400

    Add consultants section

--- wp-content/themes/pcf/template-services.php
+++ wp-content/themes/pcf/template-services.php
@@ -96,4 +96,29 @@ get_header(); ?>
 	</div>
 </section>
 
+<section class="contact-icons about-us">
+	<div class="container">
+		<div class="col-sm-4 contact-icon-col">
+			<div class="contact-icon-wrap">
+				<h2>Office</h2>
+				<h3>Bldg 7, Street N606, JAFZA Dubai</h3>
+			</div>
+		</div>
+
+		<div class="col-sm-4 contact-icon-col">
+			<div class="contact-icon-wrap">
+				<h2>Phone</h2>
+				<h3><?php the_field('phone' , 'options'); ?></h3>
+			</div>
+		</div>
+
+		<div class="col-sm-4 contact-icon-col">
+			<div class="contact-icon-wrap">
+				<h2>E-mail</h2>
+				<h3><a href="mailto:<?php the_field('email' , 'options'); ?>"><?php the_field('email' , 'options'); ?></a></h3>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php get_footer(); ?>
--- wp-content/themes/pcf/woocommerce/archive-product.php
+++ wp-content/themes/pcf/woocommerce/archive-product.php
@@ -150,6 +150,31 @@ $cat = $wp_query->get_queried_object();
 		</div><!-- .container -->
 	</div><!-- .product-listing -->
 
+	<section class="contact-icons about-us">
+		<div class="container">
+			<div class="col-sm-4 contact-icon-col">
+				<div class="contact-icon-wrap">
+					<h2>Office</h2>
+					<h3>Bldg 7, Street N606, JAFZA Dubai</h3>
+				</div>
+			</div>
+
+			<div class="col-sm-4 contact-icon-col">
+				<div class="contact-icon-wrap">
+					<h2>Phone</h2>
+					<h3><?php the_field('phone' , 'options'); ?></h3>
+				</div>
+			</div>
+
+			<div class="col-sm-4 contact-icon-col">
+				<div class="contact-icon-wrap">
+					<h2>E-mail</h2>
+					<h3><a href="mailto:<?php the_field('email' , 'options'); ?>"><?php the_field('email' , 'options'); ?></a></h3>
+				</div>
+			</div>
+		</div>
+	</section>
+
 <?php
 	/**
 	 * woocommerce_after_main_content hook.
